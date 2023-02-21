68ead91e47a82ebbcd361620c5c7593257a890c6
Evelina Garcia <101986765+egarcia8@users.noreply.github.com>
2023-02-21 13:18:05 -0600

    Add form and griz size validation

--- WordSearchGenerator/wwwroot/js/site.js
+++ WordSearchGenerator/wwwroot/js/site.js
@@ -2,3 +2,39 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+$(document).ready(function () {
+    var word = $('#userWords').val();
+    var size = $('#gridSize').val();
+
+    $('#createForm').validate({
+        rules: {
+            gridSize: {
+                required: true,
+                range:[5, 20]
+            }
+        },
+        messages: {
+            gridSize: {
+                required: "Please enter a size.",
+                range: "Please enter a number from 5-20."
+            }
+        }
+    });
+
+    window.addToWordList = function () {
+        const isValid = $('#createForm').valid();
+
+        if (isValid) {
+            alert("Good");
+        }
+        else {
+            alert("Not Good")
+        }
+
+
+
+        $('#wordList').append('<li>' + word + '</li>');
+    }
+});
+
