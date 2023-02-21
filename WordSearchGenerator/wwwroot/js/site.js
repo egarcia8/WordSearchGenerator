b917d0af889bdf38e3d431c238fd2ef4badd7f46
Evelina Garcia <101986765+egarcia8@users.noreply.github.com>
2023-02-21 15:52:58 -0600

    Add word validation and word list functionality

--- WordSearchGenerator/wwwroot/js/site.js
+++ WordSearchGenerator/wwwroot/js/site.js
@@ -4,14 +4,21 @@
 // Write your JavaScript code.
 
 $(document).ready(function () {
-    var word = $('#userWords').val();
-    var size = $('#gridSize').val();
+    
+    jQuery.validator.addMethod('lettersonly', function (value, element) {
+        return this.optional(element) || /^[a-z]+$/i.test(value);
+    }, "Letters only and no spaces please");
 
-    $('#createForm').validate({
+    $('#createWordSearchForm').validate({
         rules: {
             gridSize: {
                 required: true,
                 range:[5, 20]
+            },
+            userWords: {
+                required: true,
+                lettersonly: true
+
             }
         },
         messages: {
@@ -22,19 +29,27 @@ $(document).ready(function () {
         }
     });
 
-    window.addToWordList = function () {
-        const isValid = $('#createForm').valid();
-
+    window.addToWordList = function() {
+        const isValid = $('#createWordSearchForm').valid();
+        const word = $('#userWords').val();
+        const size = $('#gridSize').val();
         if (isValid) {
-            alert("Good");
+            
+            const wordLength = $('#userWords').val().length;
+            if (wordLength < size) {
+                $('#wordList').append('<li>' + word + '</li>');
+                $('#userWords').val("");
+                $('#gridSize').prop('disabled', true);
+                $('#submitFormButton').prop('disabled', false);
+                $('#wordInput p').empty();
+            }
+            else {
+                const message = "That word is too long."
+                $('<p>' + message + '</p>').appendTo('#wordInput');
+            }
         }
-        else {
-            alert("Not Good")
-        }
-
-
-
-        $('#wordList').append('<li>' + word + '</li>');
     }
+
+   
 });
 
