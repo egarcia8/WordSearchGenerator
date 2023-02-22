8bf5a04bd08bf4c11ce0eaa11664ff63b38a6647
Evelina Garcia <101986765+egarcia8@users.noreply.github.com>
2023-02-22 08:43:56 -0600

    Add Grid model

---
+++ WordSearchGenerator/Models/Grid.cs
@@ -0,0 +1,12 @@
+﻿namespace WordSearchGenerator.Models
+{
+    public class Grid
+    {
+        public int sizeGrid { get; set; }
+        public int MyProperty { get; set; }
+        public List<string> wordList { get; set; }
+        public List<string> wordFit { get; set; }
+        public List<string> wordNoFit { get; set; }
+
+    }
+}
